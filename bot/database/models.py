8c2de192264fb4986afca96db2feae3da953e041
Maxim Vinokurov <vmstr8@yandex.ru>
2025-05-08 20:12:57 +0300

    Незначительные изменения в модели.

--- bot/database/models.py
+++ bot/database/models.py
@@ -4,11 +4,12 @@ from tortoise import fields
 
 class Event(Model):
     id = fields.IntField(pk=True)
-    user_id = fields.IntField(unique=True)
+    user_id = fields.CharField(max_length=255, null=True, default=None)
     name = fields.CharField(max_length=255)
     car = fields.CharField(max_length=255)
-    plate_number = fields.CharField(max_length=255, unique=True)
+    plate_number = fields.CharField(max_length=255)
     phone_number = fields.CharField(max_length=255)
+    vip = fields.CharField(max_length=255, null=True, default=None)
 
     class Meta:
         table = 'events'
