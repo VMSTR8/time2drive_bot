cba5ac1145774a52de042a425de0d728999e9524
Maxim Vinokurov <vmstr8@yandex.ru>
2025-05-08 20:10:33 +0300

    Изменен текст приветствия чат-бота.

--- bot/handlers/start_handler.py
+++ bot/handlers/start_handler.py
@@ -8,10 +8,27 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def start_router(message: types.Message, state: FSMContext):
+async def start_router(message: types.Message, state: FSMContext) -> None:
+    """
+    Handles the /start command and sends a welcome message with usage instructions.
+
+    Args:
+        message (types.Message): The incoming message that triggered the /start command.
+        state (FSMContext): The current finite state machine context.
+
+    Behavior:
+        - Clears any previous FSM state.
+        - Sends a message explaining how to use the bot.
+        - Attaches a start keyboard for user interaction.
+
+    Return:
+        None
+    """
     await state.clear()
-    user = message.from_user.first_name
     await message.answer(
-        text=f'Привет {user}',
+        text=f'Чат-бот выполняет функцию поиска участников фестиваля '
+             f'по ФИО или по гос. номеру автомобиля.\n'
+             f'Функция поиска по гос. номеру работает по первым 6-ти '
+             f'символам гос. номера.',
         reply_markup=generate_start_keyboard()
     )
